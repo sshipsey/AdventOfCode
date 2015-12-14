0bf4e9891db8128103574aca386f4e0554d533d5
Steve Shipsey <Steven.Shipsey@novelis.com>
2015-12-14 13:50:10 -0500

    Finished day 12 in NodeJS

--- day12.js
+++ day12.js
@@ -1,18 +1,39 @@
 fs = require('fs');
+var total = 0;
 
 function day12(input) {
   jsonObject = JSON.parse(input);
-  iterateObject(jsonObject);
-}
-function iterateObject(object) {
-  for (var key in object) {
-    // Implement a BFS for this json "tree" to parse all values
-    // It's weird because some are arrays and some are objects.
-    // Maybe we can flatten this somehow?
-    console.log(key + " " + object[key].length);
-  }
+  console.log(iterateObject(jsonObject));
 }
 
+function iterateObject(object) {
+  for (var key in object) {
+    if (hasRed(object)) {
+      break;
+    }
+    if (typeof(object[key]) == 'object') {
+      iterateObject(object[key]);
+    } else {
+      if (!isNaN(object[key])) {
+        total += Number(object[key]);
+      }
+    }
+  }
+  return total;
+}
+function hasRed(object) {
+  for (var key in object) {
+    // For arrays, we don't care if they have red
+    if (Array.isArray(object)) {
+      return false;
+    }
+    // For objects, we do
+    if (object[key] == "red") {
+      return true;
+    }
+  }
+  return false;
+}
 jsonData = fs.readFile('C:\\Development\\AdventOfCode\\inputs\\day12.txt', 'utf8', function(err, data) {
   if (err) {
     return console.log(err);
