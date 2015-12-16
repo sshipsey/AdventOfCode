dba3d7872d0df47c194f961605e50e7325d6ff13
Steve Shipsey <sashipsey@gmail.com>
2015-12-16 00:57:03 -0500

    Dictionary comprehension is overwriting first sorted tuple for every seated pair.

--- day13.py
+++ day13.py
@@ -1,24 +1,29 @@
 from itertools import izip, permutations, tee
 def day13(input):
+  
   operatorLookup = {
   'gain' : '',
   'lose' : '-'
   }
+  
   inputArr = [str.split() for str in input]
   peopleLookup = {tuple(sorted([stringArr[0], stringArr[-1][:-1]])) : eval(operatorLookup[stringArr[2]] + stringArr[3]) for stringArr in inputArr} 
   allPeople = list(set([str[0] for str in inputArr] + [str[-1][:-1] for str in inputArr]))
 
   allPerms = permutations(allPeople)
   maxHappiness = -1
-  
+  for p in peopleLookup:
+    print(p)  
   for arrangement in allPerms:
     thisArrangement = 0
 
-    for i in range(0, len(arrangement) - 1):
-      a,b = tee(arrangement)
-      next(b, None)
-      izip(a,b)
-      thisArrangement = sum(peopleLookup[tuple(sorted([arrangement]))])
-       
+    a,b = tee(arrangement)
+    next(b, None)
+    thisArrangement = sum([peopleLookup[tuple(sorted(x))] for x in izip(a,b)]) + peopleLookup[tuple(sorted([arrangement[0], arrangement[-1]]))]
+
+    if thisArrangement > maxHappiness or maxHappiness == -1:
+      maxHappiness = thisArrangement
+  return maxHappiness 
+
 input = open(r'C:\Development\AdventOfCode\inputs\day13.txt').read().split('\n')
-day13(input)
+print(day13(input))
