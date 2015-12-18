03ae00b1577d4e0d3ad6b613708f55a717e26c3d
Steve Shipsey <Steven.Shipsey@novelis.com>
2015-12-18 14:36:47 -0500

    Day 14 part 1

---
+++ day14.py
@@ -0,0 +1,11 @@
+import re
+from itertools import chain, cycle
+
+def day14(input):
+    speeds = [re.findall("(\d+)", reindeer) for reindeer in input]
+    reindeerMap = [cycle(chain([int(s[0])] * int(s[1]), [0] * int(s[2]))) for s in speeds]
+    distance = max([sum([next(m, None) for x in xrange(0,2503)]) for m in reindeerMap])
+    return distance
+
+input = open(r'C:\Development\AdventOfCode\inputs\day14.txt').read().split('\n')
+print(day14(input))
