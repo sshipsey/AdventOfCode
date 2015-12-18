171c83a422932e6f10deae1e4c55e7aefae44cb6
Steve Shipsey <Steven.Shipsey@novelis.com>
2015-12-18 14:37:40 -0500

    naming

--- day14.py
+++ day14.py
@@ -3,9 +3,9 @@ from itertools import chain, cycle
 
 def day14(input):
     speeds = [re.findall("(\d+)", reindeer) for reindeer in input]
-    reindeerMap = [cycle(chain([int(s[0])] * int(s[1]), [0] * int(s[2]))) for s in speeds]
-    distance = max([sum([next(m, None) for x in xrange(0,2503)]) for m in reindeerMap])
-    return distance
+    reideerMap = [cycle(chain([int(s[0])] * int(s[1]), [0] * int(s[2]))) for s in speeds]
+    maxDistance = max([sum([next(m, None) for x in xrange(0,2503)]) for m in reindeerMap])
+    return maxDistance
 
 input = open(r'C:\Development\AdventOfCode\inputs\day14.txt').read().split('\n')
 print(day14(input))
