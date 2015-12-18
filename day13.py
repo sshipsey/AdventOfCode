22b1ca2bf42a8e43db42c7dd73a35b94d184137b
Steve Shipsey <Steven.Shipsey@novelis.com>
2015-12-18 13:09:05 -0500

    Day 13 part 1 complete

--- day13.py
+++ day13.py
@@ -7,22 +7,22 @@ def day13(input):
   }
   
   inputArr = [str.split() for str in input]
-  peopleLookup = {tuple(sorted([stringArr[0], stringArr[-1][:-1]])) : eval(operatorLookup[stringArr[2]] + stringArr[3]) for stringArr in inputArr} 
+  peopleLookup = {tuple([stringArr[0], stringArr[-1][:-1]]) : eval(operatorLookup[stringArr[2]] + stringArr[3]) for stringArr in inputArr} 
   allPeople = list(set([str[0] for str in inputArr] + [str[-1][:-1] for str in inputArr]))
 
   allPerms = permutations(allPeople)
   maxHappiness = -1
-  for p in peopleLookup:
-    print(p)  
-  for arrangement in allPerms:
+
+  for q in allPerms:
     thisArrangement = 0
-
-    a,b = tee(arrangement)
+    a,b = tee(q)
     next(b, None)
-    thisArrangement = sum([peopleLookup[tuple(sorted(x))] for x in izip(a,b)]) + peopleLookup[tuple(sorted([arrangement[0], arrangement[-1]]))]
+    z = izip(a,b)
+    thisArrangement = sum([peopleLookup[x] + peopleLookup[tuple(reversed(x))] for x in z]) + sum([peopleLookup[tuple([q[0],q[-1]])], peopleLookup[tuple([q[-1],q[0]])]])
 
-    if thisArrangement > maxHappiness or maxHappiness == -1:
+    if thisArrangement > maxHappiness or maxHappiness is -1:
       maxHappiness = thisArrangement
+
   return maxHappiness 
 
 input = open(r'C:\Development\AdventOfCode\inputs\day13.txt').read().split('\n')
