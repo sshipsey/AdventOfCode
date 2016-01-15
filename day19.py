e7ea04a4655b1381c753df6387cf50dc83a960db
Steven Shipsey <steven.shipsey@novelis.com>
2016-01-15 14:35:45 -0500

    Started Day 19

--- day19.py
+++ day19.py
@@ -1,5 +1,17 @@
+import re
+
 def day19(input):
-  return input
+  molecule = input[-1]
+  transformations = input[:-2]
+  uniqueE = []
+  for t in transformations:
+    thisT = t.split(" ")[0]
+    if thisT not in uniqueE:
+      uniqueE.append(thisT)
+  for x in uniqueE:
+    print(x)
+  molList = re.findall('[A-Z][^A-Z]*', molecule)
+  print(molList)
 
 input = open(r'C:\Development\AdventOfCode\inputs\day19.txt').read().split('\n')
 print(day19(input))
